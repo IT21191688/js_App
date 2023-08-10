3c181619078d3b7a48d64a952cf4e16eacbe822c
IT21191688 <IT21191688@my.sliit.lk>
2023-08-10 09:32:17 +0530

    final

---
+++ js/customer.js
@@ -0,0 +1,34 @@
+const database = firebase.firestore();
+
+function createCustomer(nic, name, address) {
+    database.collection('customer').add({
+        nic: nic,
+        name: name,
+        address: address
+    })
+        .then(result => {
+            console.log(result);
+            alert("Success")
+        })
+        .catch(error => {
+            console.log(error);
+            alert("not success")
+        })
+}
+
+createCustomer("98", "nimal", "colombo");
+
+function saveData() {
+
+    let nicData = document.getElementById('nic');
+    let nameData = document.getElementById('name');
+    let addressData = document.getElementById('address');
+
+    let nic = nicData.value;
+    let name = nameData.value;
+    let address = addressData.value;
+    createCustomer(nic, name, address);
+}
+
+
+
---
+++ js/item.js
@@ -0,0 +1,25 @@
+let database = firebase.firestore();
+
+function createItem(description, unitPrice, qty) {
+    database.collection('item').add({
+        description: description,
+        unitPrice: unitPrice,
+        qty: qty
+    })
+        .then(result => {
+            console.log(result);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+function saveData() {
+    let descData = document.getElementById('description');
+    let priceData = document.getElementById('unitprice');
+    let qtyData = document.getElementById('qty');
+
+    let desc = descData.value;
+    let price = priceData.value;
+    let qty = qtyData.value;
+    createItem(desc, price, qty);
+}
